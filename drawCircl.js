a5b1ef667893ff05ab23662a93fa3c0b377346ff
deepvn98 <deepvn988@gmail.com>
2021-03-31 16:12:20 +0700

    module1

--- drawCircl.js
+++ drawCircl.js
@@ -16,8 +16,3 @@ class Circl{
 }
 let circl = new Circl(10,10,100,"#000000" );
 circl.Render()
-// let c = document.getElementById("myCanvas");
-// let ctx = c.getContext("2d");
-// ctx.beginPath();
-// ctx.arc(95, 50, 40, 0, 2 * Math.PI);
-// ctx.stroke();
